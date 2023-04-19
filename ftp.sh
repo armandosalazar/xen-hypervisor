968780f579497d224190c88007866c91decab79f
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2023-04-18 23:27:33 -0600

    update dependencies

--- ftp.sh
+++ ftp.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 apt install vsftpd -y
+
+# add users and set password
+adduser armando
+passwd armando
+mkdir {/home/armando, /home/armando/ftp}
+sudo chown nobody:nogroup /home/armando/ftp
+# remove write permissions
+sudo chmod a-w /home/armando/ftp
--- http.sh
+++ http.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 apt update
-apt install apache2 php -y
-echo "<?php phpinfo() ?>" > /var/www/html/prueba.php
+apt install apache2 php php-mysql -y
+echo "<?php
+	phpinfo()
+?>" > /var/www/html/prueba.php
 php -v
 echo "Successfully installation!"
 
@@ -10,6 +12,7 @@ echo "Successfully installation!"
 # apt install mysql-server -y
 apt install wget -y
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.22-1_all.deb
+apt install lsb-release gnupg -y
 dpkg -i mysql-apt-config_0.8.22-1_all.deb
 apt update
 apt install mysql-server
