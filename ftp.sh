8fa980ea4a0ff220f813c3f61bbbb7495bcc3198
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2023-04-20 14:09:12 -0600

    update ftp

--- ftp.sh
+++ ftp.sh
@@ -5,7 +5,8 @@ apt install vsftpd -y
 # add users and set password
 adduser armando
 passwd armando
-mkdir {/home/armando, /home/armando/ftp}
-sudo chown nobody:nogroup /home/armando/ftp
+mkdir /home/armando
+mkdir /home/armando/ftp
+# sudo chown nobody:nogroup /home/armando/ftp
 # remove write permissions
-sudo chmod a-w /home/armando/ftp
+# sudo chmod a-w /home/armando/ftp
