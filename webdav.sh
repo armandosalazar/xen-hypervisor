cda3530174366a4f597363b71879bbb916ee974b
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2023-04-20 14:00:02 -0600

    update webdav

--- webdav.sh
+++ webdav.sh
@@ -23,3 +23,15 @@ systemctl restart apache2
 htdigest -c /etc/apache2/users.password webdav armando
 # create a user and define password to access webdav from basic*
 htpasswd -c /etc/apache2/.users.password armando
+
+# use authentication
+nano /etc/apache2/sites-available/000-default.conf # edit this file and add:
+
+Alias /webdav /var/www/webdav
+<Directory /var/www/webdav>
+	DAV On
+	AuthType Basic
+	AuthName "webdav"
+	AuthUserFile /etc/apache2/users.password
+	Require valid-user
+</Directory>
