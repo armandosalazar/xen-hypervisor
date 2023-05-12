a25746515a10a9711f4b390be91248aa078e7b03
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2023-05-12 09:29:07 -0600

    add ssh installed

--- browse-files.sh
+++ browse-files.sh
@@ -7,3 +7,18 @@ done
 for file in *; do
 	echo $file
 done
+
+echo -n "Introduce una opción: "
+read opt
+case opt in
+	1)
+		echo "Opción uno"
+		;;
+	2)
+		echo "Opción dos"
+		;;
+	\?)
+		echo "Opción invalida"
+		exit
+		;;
+esac
